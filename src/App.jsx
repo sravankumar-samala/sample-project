e8db9a1d2d5c01b38f5a79a6dfd650233bb733d5
sravankumar-samala <sravankumarsamala20@gmail.com>
2023-10-16 16:19:53 +0530

    Modified app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+const Home = () => <h1>This is Home page</h1>;
+
+const About = () => <h1>This is About page</h1>;
+
+const NotMatch = () => <h1>No match found</h1>;
+
 function App() {
-  return <h1>This is vite project</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotMatch />} />
+    </Routes>
+  );
 }
 
 export default App;
